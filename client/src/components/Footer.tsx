cd3ab48173c1cfcfb17e1e2b4c596d112f8d5208
Cody <subliminalverses2@gmail.com>
2022-11-22 09:44:38 -0600

    change navbar length

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="bg-zinc-800">
-      <div className=" w-9/12 m-auto flex items-center justify-center py-3">
+      <div className=" w-11/12 m-auto flex items-center justify-center py-3">
         <p className="text-white">
           Cody Clements Copyright &copy; {new Date().getFullYear()}
         </p>
