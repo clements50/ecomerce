09ed6734a8d42e087c668682ce6adfbc22860563
Cody <subliminalverses2@gmail.com>
2022-12-04 21:37:21 -0600

    add circle icons to carousel buttons

--- client/src/components/CarouselButtons.tsx
+++ client/src/components/CarouselButtons.tsx
@@ -1,18 +1,19 @@
+import { FaChevronCircleLeft, FaChevronCircleRight } from "react-icons/fa";
 const CarouselButtons = ({ children, dispatch }: any) => {
   return (
     <>
-      <button
-        className=" text-2xl absolute top-1/2 right-4 bg-gray-100 rounded-full h-8 w-8"
+      <FaChevronCircleRight
+        className=" text-2xl absolute top-1/2 right-4 bg-gray-100 rounded-full h-8 w-8 cursor-pointer"
         onClick={() => dispatch({ type: "increment", payload: children })}
       >
         &rarr;
-      </button>
-      <button
-        className="absolute text-2xl left-4 top-1/2 bg-gray-100 rounded-full h-8 w-8 "
+      </FaChevronCircleRight>
+      <FaChevronCircleLeft
+        className="absolute text-2xl left-4 top-1/2 bg-gray-100 rounded-full h-8 w-8 cursor-pointer"
         onClick={() => dispatch({ type: "decrement", payload: children })}
       >
         &larr;
-      </button>
+      </FaChevronCircleLeft>
     </>
   );
 };
